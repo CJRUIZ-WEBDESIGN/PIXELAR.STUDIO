af0abfcda96604b5231d0a17cb0165c2b67baa06
Cristian Ruiz <cjruiz2013@gmail.com>
2023-07-30 23:29:11 -0300

    Se incorporo JSON, se rediseño la card de Servicios, se mejoro la visualizacion y las se cambio fuente, se muestran de forma dinamica mediante el DOM y se llama a JSON con FETCH, resta: aplicar colores finales del sitio

---
+++ js/app.js
@@ -0,0 +1,146 @@
+const URL = "js/servicios.json"
+const carrito = [];
+const servicios = [];
+
+function cargarServicios() {
+    container.innerHTML = "";
+    servicios.forEach((servicio) => {
+        container.innerHTML += cardReturn(servicio);
+    });
+  }
+  
+async function getServiciosAsync() {
+    const response = await fetch(URL)
+    const data = await response.json()
+    servicios.push(...data)
+    cargarServicios()
+}
+getServiciosAsync()
+
+
+function mostrarServiciosCarrito() {
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    let carritoDetalle = "";
+    let total = 0;
+
+    if (carrito.length > 0) {
+        carrito.forEach((servicio, index) => {
+            carritoDetalle += `
+                <div class="carritoItem">
+                    <img src="${servicio.img}" alt="...">
+                    <p><b>ID: </b>${servicio.id}</p>
+                    <p><b>NOMBRE: </b>${servicio.nombre}</p>
+                    <p><b>PRECIO: </b>$${servicio.precio}</p>
+                    <p><b>CANTIDAD: </b>${servicio.cantidad}</p>
+                    <button class="eliminar" data-index="${index}"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                </div>`;
+            total += servicio.precio * servicio.cantidad;
+        });
+        carritoDetalle += `
+                <p class="total">TOTAL: $${total}</p>`;
+    } else {
+        carritoDetalle = "<b>El carrito está vacío.</b>";
+    }
+    document.querySelector('.carritoContainer').innerHTML = carritoDetalle;
+    document.querySelectorAll('.eliminar').forEach(button => button.addEventListener('click', eliminarProducto));
+    document.querySelector('.finalizar-compra').addEventListener('click', finalizarCompra);
+}
+
+mostrarServiciosCarrito()
+
+function agregarAlCarrito(servicio) {
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    let servicioEncontrado = carrito.find(serv => serv.id === servicio.id);
+    if (servicioEncontrado) {
+        servicioEncontrado.cantidad++;
+    } else {
+        servicio.cantidad = 1;
+        carrito.push(servicio);
+    }
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    Swal.fire({
+        title: 'Agregado al Carrito!',
+        text: `Agregaste ${servicio.nombre} correctamente.`,
+        imageUrl: `${servicio.img}`,
+        imageWidth: 250,
+        imageHeight: 250,
+        imageAlt: 'Imagen de Producto',
+        showClass: {
+            popup: 'fondo_oscuro animate__animated animate__backInDown'
+        },
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        hideClass: {
+            popup: 'fondo_oscuro animate__animated animate__backOutDown'
+        }
+    })
+    mostrarServiciosCarrito();
+}
+
+async function eliminarProducto(event) {
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    carrito.splice(event.target.dataset.index, 1);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-right',
+        iconColor: 'white',
+        customClass: {
+          popup: 'colored-toast'
+        },
+        showConfirmButton: false,
+        timer: 800,
+        timerProgressBar: true
+      })
+      await Toast.fire({
+        icon: 'success',
+        text: "Se ha eliminado el producto del carrito",
+      })
+    mostrarServiciosCarrito();
+}
+
+container.addEventListener('click', (event) => {
+    if (event.target.classList.contains('botonCarrito')) {
+        const servId = event.target.dataset.id;
+        const servicio = servicios.find((servicio) => servicio.id === (servId));
+        agregarAlCarrito(servicio);
+    }
+});
+const botonFinalizar = document.querySelector('.finalizar-compra');
+if (botonFinalizar) {
+    botonFinalizar.addEventListener('click', finalizarCompra);
+}
+
+async function finalizarCompra() {
+    const pasos = ['1', '2']
+    const finCompra = Swal.mixin({
+        progressSteps: pasos,
+        showConfirmButton: false,
+        timer: 2800,
+        timerProgressBar: true,
+        showClass: { backdrop: 'swal2-noanimation' },
+        hideClass: { backdrop: 'swal2-noanimation' }
+    })
+    await finCompra.fire({
+        title: '<b>Su compra finalizo con exito!</b> Muchas gracias por visitarnos',
+        currentProgressStep: 0,
+        showClass: { backdrop: 'swal2-noanimation' },
+    })
+    await finCompra.fire({
+        title: '<b>CR Diseño Web</b> Le agradece por su Visita!',
+        currentProgressStep: 2,
+        showConfirmButton: false,
+        timer: 1800,
+        timerProgressBar: true,
+        showClass: { backdrop: 'swal2-noanimation' },
+    })
+    localStorage.removeItem("carrito");
+
+    window.location.href = "index.html"
+
+    mostrarServiciosCarrito();
+}
+
+cargarServicios()
+
---
+++ js/index.js
@@ -0,0 +1,105 @@
+const container = document.querySelector("#container");
+const buscarBtn = document.querySelector("#buscarBtn");
+const busquedaInput = document.querySelector("#busquedaInput");
+const carritoContainer = document.querySelector("#carritoContainer");
+const vaciarCarritoBtn = document.querySelector('#vaciarCarritoBtn');
+const gridContainer = document.querySelector('.grid-container');
+
+function cardReturn(servicio) {
+  return `   <div class="grid-container">
+
+
+        <div class="card">
+
+            <div class="card__details">
+                <div class="card__side card__side--front-1">
+                    <div class="card__title card__title--1">
+                        <img src="${servicio.img}" class="imagProduc" alt="Imagen Ilustrativa">
+                        <h4 class="card__heading">${servicio.nombre}</h4>
+                    </div>
+                    <ul>
+                        <li>${servicio.correo}</li>
+                        <li>${servicio.dominio}</li>
+                        <li>${servicio.hosting}</li>
+                        <li>${servicio.ssl}</li>
+                        <li>${servicio.responsive}</li>
+                        <li>${servicio.section}</li>
+                        <li>${servicio.mapa}</li>
+                        <li>${servicio.wsp}</li>
+                        <li>${servicio.seo}</li>
+                        <li>${servicio.formulario}</li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="card__side card__side--back card__side--back-1">
+                <div class="card__cta">
+                    <div class="card__price-box">
+                        <li class="p-det">${servicio.descripcion}</li>
+
+                        <p class="card__price-value">$ ${servicio.precio}</p>
+                    </div>
+         
+                    <li class="p-det">Desarrollado con tecnologia:</li>
+                    
+                <div class="container-skill skill__class">
+
+                    <div class="skill__class--icons1">
+                        <li><i class="fa-brands fa-html5"></i></li>
+                        </div>
+
+                    <div class="skill__class--icons2">
+                        <li><i class="fa-brands fa-css3"></i></li>
+
+                    </div>
+
+                    <div class="skill__class--icons3">
+                        <li><i class="fa-brands fa-js"></i></li>
+
+                    </div>
+                </div>
+
+                    <button class="botonCarrito btn btn--white" data-id="${servicio.id}">agregar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+  
+`;
+}
+
+function vaciarCarrito() {
+  localStorage.removeItem('carrito');
+  mostrarServiciosCarrito();
+  let timerInterval
+  Swal.fire({
+    title: 'Vaciando Carrito!',
+    html: 'El carrito se vaciara en <b></b> milisegundos.',
+    timer: 1500,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading()
+      const b = Swal.getHtmlContainer().querySelector('b')
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft()
+      }, 100)
+    },
+    willClose: () => {
+      clearInterval(timerInterval)
+    }
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+    }
+  })
+}
+vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+buscarBtn.addEventListener("click", () => {
+  const searchTerm = busquedaInput.value.toLowerCase().trim();
+  const resultados = servicios.filter((servicio) =>
+    servicio.nombre.toLowerCase().includes(searchTerm));
+
+  container.innerHTML = resultados.length > 0 ? resultados.map(servicio => cardReturn(servicio)).join("") : "<b>No se encontraron productos.</b>";
+});
+
